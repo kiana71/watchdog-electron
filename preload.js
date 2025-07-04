7450e2a9719acd7eb33ef495a0fea2c2a26eef56
kiana71 <gobishanthan04@gmail.com>
2025-07-04 09:21:48 -0400

    new changes in manula installation

--- preload.js
+++ preload.js
@@ -21,13 +21,13 @@ const computerName = os.hostname();
 contextBridge.exposeInMainWorld('api', {
   send: (channel, data) => {
     // whitelist channels
-    const validChannels = ['app-quit', 'app-minimize', 'restart-service', 'save-client-name', 'get-client-name', 'check-for-updates', 'download-update', 'install-update', 'set-auto-update', 'get-auto-update-setting'];
+    const validChannels = ['app-quit', 'app-minimize', 'restart-service', 'save-client-name', 'get-client-name', 'check-for-updates', 'download-update', 'install-update'];
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data);
     }
   },
   receive: (channel, func) => {
-    const validChannels = ['client-log', 'client-error', 'client-status', 'client-name-loaded', 'update-status', 'auto-update-setting-loaded'];
+    const validChannels = ['client-log', 'client-error', 'client-status', 'client-name-loaded', 'update-status'];
     if (validChannels.includes(channel)) {
       // Deliberately strip event as it includes `sender` 
       ipcRenderer.on(channel, (event, ...args) => func(...args));
